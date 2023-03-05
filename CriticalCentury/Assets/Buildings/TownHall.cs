90d44ba4ff1e719c0a6d180d3b4a5f3783dffc92
Oliver Clarke <olivercameronclarke@gmail.com>
2023-03-05 05:57:43 -0500

    dasdsadsddsasaddsasd UI

--- CriticalCentury/Assets/Buildings/TownHall.cs
+++ CriticalCentury/Assets/Buildings/TownHall.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TownHall : Building
 {
+    public List<TownHallUpgrade> upgrade_buttons;
+    public List<int> queued_upgrades;
+
+    public TextMeshProUGUI building_lvl;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PopulateUpgrades();  
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void NextYear()
     {
+        foreach(int index in queued_upgrades)
+        {
+            upgrade_buttons[index].time_remaining -= 1;
+            if (upgrade_buttons[index].time_remaining == 0)
+            {
+                AddUpgrade(available_upgrades[index]);
+                available_upgrades.Remove(available_upgrades[index]);          
+            }
+        }
+        PopulateUpgrades();
+    }
+    
+    public void PopulateUpgrades()
+    {
+        int index = 0;
+        building_lvl.text = "Building Level: " + building_level;
         
+        foreach(Upgrade upgrade in available_upgrades)
+        {
+            if (upgrade_buttons[index].time_remaining == 0)
+                queued_upgrades.Remove(index); 
+                
+            if (queued_upgrades.Contains(index))
+            {
+                upgrade_buttons[index].PurchaseUpgrade();
+                index += 1;
+                continue;
+            }
+            bool canAfford = TryUpgrade(index);
+            upgrade_buttons[index].UpdateUpgrade(upgrade, canAfford);
+            index += 1;
+        }
+        while (index < 4)
+        {
+            upgrade_buttons[index].NoUpgrade();
+            index += 1;
+        }
+    }
+
+    public void TryPurchaseUpgrade(int index)
+    {
+        if(TryUpgrade(index) && !queued_upgrades.Contains(index))
+        {
+            PayUpgrade(available_upgrades[index].upgrade_cost);  
+            PopulateUpgrades();
+            queued_upgrades.Add(index);
+            upgrade_buttons[index].PurchaseUpgrade();                   
+        }
     }
 }
---
+++ CriticalCentury/Assets/TownHallUpgrade.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TownHallUpgrade : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI upgrade_title;
+    [SerializeField] private TextMeshProUGUI upgrade_description;
+    [SerializeField] private TextMeshProUGUI upgrade_status; 
+    [SerializeField] private Image upgrade_background;
+    [SerializeField] private Button button;
+    
+    public int time_remaining;
+
+    private void Start() 
+    {
+        upgrade_background = this.gameObject.transform.GetChild(0).gameObject.GetComponent<Image>();
+        upgrade_title = this.gameObject.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
+        upgrade_description = this.gameObject.transform.GetChild(2).gameObject.GetComponent<TextMeshProUGUI>();
+        upgrade_status = this.gameObject.transform.GetChild(3).gameObject.GetComponent<TextMeshProUGUI>();
+        button = this.gameObject.transform.GetChild(0).gameObject.GetComponent<Button>();
+    }
+
+    public void UpdateUpgrade(Upgrade upgrade, bool canAfford)
+    {
+        upgrade_title.text = upgrade.upgrade_name;
+        upgrade_description.text = upgrade.upgrade_description;
+        upgrade_status.text = "$" + upgrade.upgrade_cost + "   " + upgrade.upgrade_duration + " year(s)";
+        time_remaining = upgrade.upgrade_duration;
+
+        if (canAfford)
+        {
+            upgrade_background.color = new Color32(255,255,255,255);
+        }
+        else
+        {
+            upgrade_background.color = new Color32(196,196,196,255);
+        }
+    }
+
+    public void NoUpgrade()
+    {
+        upgrade_title.text = "";
+        upgrade_description.text = "Upgrade your Town Hall to unlock more upgrades!";
+        upgrade_status.text = "";
+        upgrade_background.color = new Color32(255,255,255,150);
+        button.interactable = false;
+    }
+
+    public void PurchaseUpgrade()
+    {
+        upgrade_status.text = "Time remaining: " + time_remaining + " years.";
+        upgrade_background.color = new Color32(171,138,138,255);
+    }
+}
